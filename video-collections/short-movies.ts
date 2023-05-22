0fc4ee6b53f6cf3e33c4ecec357eeaac20f07192
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2023-05-22 22:43:24 +0100

    New short movie

--- video-collections/short-movies.ts
+++ video-collections/short-movies.ts
@@ -3,4 +3,5 @@ import { VideoType } from "~/type/video-type";
 export const shortMovies: VideoType[] = [
   { code: "UUlaseGrkLc", description: "nice" },
   { code: "1P3ZgLOy-w8", description: "tartaruga" },
+  { code: "kQjtK32mGJQ", description: "work work work" },
 ];
