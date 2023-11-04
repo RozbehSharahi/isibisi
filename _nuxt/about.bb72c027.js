2dc6ba1d66b45fb9d8ce5b64390ef2e339a17e43
RozbehSharahi <rozbeh.sharahi@gmail.com>
2023-11-04 20:00:26 +0000

    Deploying to live from @ RozbehSharahi/isibisi@5c13d0e6551280b2438fe9843f00fdb1b6784dad 🚀

---
+++ _nuxt/about.bb72c027.js
@@ -0,0 +1,1 @@
+import{a as t}from"./entry.7ade541a.js";const r={};function c(e,o){return"about"}const n=t(r,[["render",c]]);export{n as default};
--- _nuxt/about.e73872fc.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./entry.19e62a57.js";const r={};function c(e,o){return"about"}const n=t(r,[["render",c]]);export{n as default};
